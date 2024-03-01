b914fb16e8d3900177e3e86f68710a381acd8e4c
Shoro749 <romanbro749@gmail.com>
2024-03-01 15:14:16 +0200

    Add project files.

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1Study.ViewModels.Base;
+
+namespace WpfApp1Study.ViewModels
+{
+    internal class MainViewModel : ViewModelBase
+    {
+        public ObservableCollection<string> Names { get; set; }
+
+        public MainViewModel()
+        {
+            Names = new ObservableCollection<string>();
+        }
+    }
+}
